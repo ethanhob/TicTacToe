c914d46c7f52a15e84f5db6a2dbdc0c325048662
ethanhob <95361850+ethanhob@users.noreply.github.com>
2023-07-30 12:29:15 +1000

    initial upload
    initial upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TicTacToe from './TicTacToe/TicTacToe';
+
+function App() {
+  return (
+    <div className="App">
+      <TicTacToe />
+      {/* make sure components start with capital letters */}
+    </div>
+  );
+}
+
+export default App;
